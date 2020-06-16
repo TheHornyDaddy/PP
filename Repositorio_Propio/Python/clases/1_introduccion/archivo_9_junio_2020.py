6b1c8fc374310af21bcc7684a3e3b5285e00cd16
TheHornyDaddy <erne.rob1789@gmail.com>
2020-06-16 18:20:14 -0500

    Test

---
+++ Repositorio_Propio/Python/clases/1_introduccion/archivo_9_junio_2020.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a = np.array([1,2,3])
+b = np.array([1,1,1])
+#c = np.
+
+def mi_funcion(a,b):
+  return 0
+  
+mi_funcion(a,b)
+print("Variable: ", a)
+
+
+
+ 
+def otra_funcion():
+  return("Hola")
